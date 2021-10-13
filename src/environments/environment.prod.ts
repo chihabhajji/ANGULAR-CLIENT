b75b5afc203afd47cf47c369e0cbdb2de121a26b
chihabhajji <blackpang23@gmail.com>
2021-10-13 21:19:06 +0100

    fixed ur typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   gateway: 'https://fgsc-gateway.herokuapp.com',
   apiGateway: 'https://fgsc-gateway.herokuapp.com/api',
-  notificationsRSocketUrl: 'http://localhost:7000/rs'
+  notificationsRSocketUrl: 'ws://localhost:7000/rs'
 };
